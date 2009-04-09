368fa537a507756d94544f20f8ae44338f0510e0
joschmidt <joschmidt@11aa7465-d437-4035-867d-07b0d7ee7c12>
2009-04-09 20:07:13 +0000

    added topic map locator tests
    git-svn-id: https://quaaxtm.svn.sourceforge.net/svnroot/quaaxtm@24 11aa7465-d437-4035-867d-07b0d7ee7c12

--- trunk/tests/BasicRunTest.php
+++ trunk/tests/BasicRunTest.php
@@ -5,6 +5,11 @@ class BasicRunTest extends PHPTMAPITestCase {
   
   public function testTopicMap() {
     $this->assertTrue($this->topicMap instanceof TopicMap);
+    $tm = $this->topicMap;
+    $this->assertEquals($tm->getLocator(), self::$tmLocator);
+    $tmSystem = $this->sharedFixture;
+    $this->assertTrue(count($tmSystem->getLocators()) > 0);
+    $this->assertTrue(in_array($tm->getLocator(), $tmSystem->getLocators()));
   }
   
 }
