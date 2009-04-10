cb742eecd06ee6c937c6aca971133c1c5feedcbe
joschmidt <joschmidt@11aa7465-d437-4035-867d-07b0d7ee7c12>
2009-04-10 20:13:46 +0000

    added more tests
    git-svn-id: https://quaaxtm.svn.sourceforge.net/svnroot/quaaxtm@45 11aa7465-d437-4035-867d-07b0d7ee7c12

--- trunk/tests/NameTest.php
+++ trunk/tests/NameTest.php
@@ -33,5 +33,114 @@ class NameTest extends PHPTMAPITestCase {
   public function testTopicMap() {
     $this->assertTrue($this->topicMap instanceof TopicMap);
   }
+  
+  public function testParent() {
+    $parent = $this->topicMap->createTopic();
+    $this->assertEquals(count($parent->getNames()), 0, 
+      'Expected new topic to be created without names!');
+    $name = $parent->createName('Name');
+    $this->assertEquals($name->getParent()->getId(), $parent->getId(), 
+      'Unexpected name parent after creation!');
+    $this->assertEquals(count($parent->getNames()), 1, 'Expected 1 name!');
+    $ids = $this->getIdsOfConstructs($parent->getNames());
+    $this->assertTrue(in_array($name->getId(), $ids, true), 
+      'Name is not part of getNames()!');
+    $name->remove();
+    $this->assertEquals(count($parent->getNames()), 0, 'Expected 0 names after removal!');
+  }
+  
+  public function testDefaultNameType() {
+    $name = $this->createName();
+    $defaultType = $name->getType();
+    $sids = $defaultType->getSubjectIdentifiers();
+    $this->assertTrue(in_array('http://psi.topicmaps.org/iso13250/model/topic-name', 
+      $sids));
+  }
+  
+  public function testValue() {
+    $value1 = 'PHPTMAPI name';
+    $value2 = 'Süßer Name';
+    $name = $this->createName();
+    $this->assertTrue($name instanceof Name);
+    $name->setValue($value1);
+    $this->assertEquals($name->getValue(), $value1);
+    $name->setValue($value2);
+    $this->assertEquals($name->getValue(), $value2);
+    try {
+      $name->setValue(null);
+      $this->fail('setValue(null) is not allowed!');
+    } catch (ModelConstraintException $e) {
+      // no op.
+    }
+    $this->assertEquals($name->getValue(), $value2);
+  }
+  
+  public function testVariantValueDatatype() {
+    $name = $this->createName();
+    $this->assertTrue($name instanceof Name);
+    $theme = $this->topicMap->createTopic();
+    $dtString = 'http://www.w3.org/2001/XMLSchema#string';
+    try {
+      $name->createVariant(null, $dtString, array($theme));
+      $this->fail('null is not allowed as value!');
+    } catch (ModelConstraintException $e) {
+      // no op.
+    }
+    try {
+      $name->createVariant('Variant', null, array($theme));
+      $this->fail('null is not allowed as datatype!');
+    } catch (ModelConstraintException $e) {
+      // no op.
+    }
+    try {
+      $name->createVariant(null, null, array($theme));
+      $this->fail('Variants have a value and a datatype != null!');
+    } catch (ModelConstraintException $e) {
+      // no op.
+    }
+    $variant = $name->createVariant('Variant', $dtString, array($theme));
+    $this->assertEquals($variant->getValue(), 'Variant', 'Values are different!');
+    $this->assertEquals($variant->getDatatype(), $dtString, 'Datatypes are different!');
+    $variant = $name->createVariant('Variant Variant', $dtString, array($theme));
+    $this->assertEquals($variant->getValue(), 'Variant Variant', 'Values are different!');
+    $this->assertEquals(count($variant->getScope()), 1, 'Expected 1 theme in scope!');
+    $ids = $this->getIdsOfConstructs($variant->getScope());
+    $this->assertTrue(in_array($theme->getId(), $ids, true), 
+      'Theme is not part of getScope()!');
+  }
+  
+  public function testVariantScope() {
+    $name = $this->createName();
+    $this->assertTrue($name instanceof Name);
+    $dtString = 'http://www.w3.org/2001/XMLSchema#string';
+    $theme1 = $this->topicMap->createTopic();
+    $theme2 = $this->topicMap->createTopic();
+    $theme3 = $this->topicMap->createTopic();
+    $name->addTheme($theme1);
+    try {
+      $variant = $name->createVariant('Variant', $dtString, array($theme1));
+      $this->fail('Scope is not a true superset!');
+    } catch (ModelConstraintException $e) {
+      // no op.
+    }
+    try {
+      $variant = $name->createVariant('Variant', $dtString, array($theme2, $theme3));
+      $this->fail('Scope is not a true superset!');
+    } catch (ModelConstraintException $e) {
+      // no op.
+    }
+    try {
+      $variant = $name->createVariant('Variant', $dtString, array($theme3));
+      $this->fail('Scope is not a true superset!');
+    } catch (ModelConstraintException $e) {
+      // no op.
+    }
+    $variant = $name->createVariant('Variant', $dtString, array($theme1, $theme2));
+    $ids = $this->getIdsOfConstructs($variant->getScope());
+    $this->assertTrue(in_array($theme1->getId(), $ids, true), 
+      'Theme is not part of getScope()!');
+    $this->assertTrue(in_array($theme2->getId(), $ids, true), 
+      'Theme is not part of getScope()!');
+  }
 }
 ?>
