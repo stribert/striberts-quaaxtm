c9b04e14dfdb139fa97d2413b91412ec27a0da40
joschmidt <joschmidt@11aa7465-d437-4035-867d-07b0d7ee7c12>
2009-04-10 11:27:26 +0000

    added more tests
    git-svn-id: https://quaaxtm.svn.sourceforge.net/svnroot/quaaxtm@37 11aa7465-d437-4035-867d-07b0d7ee7c12

--- trunk/tests/tests.php
+++ trunk/tests/tests.php
@@ -23,4 +23,5 @@ require_once('BasicRunTest.php');
 require_once('AssociationTest.php');
 require_once('ConstructTest.php');
 require_once('ItemIdentifierConstraintTest.php');
+require_once('NameTest.php');
 ?>
