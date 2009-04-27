f06c28ed4e47f2ad18664147241819c110460af3
joschmidt <joschmidt@11aa7465-d437-4035-867d-07b0d7ee7c12>
2009-04-27 22:10:53 +0000

    added more tests
    git-svn-id: https://quaaxtm.svn.sourceforge.net/svnroot/quaaxtm@72 11aa7465-d437-4035-867d-07b0d7ee7c12

--- trunk/tests/TopicMapSystemTest.php
+++ trunk/tests/TopicMapSystemTest.php
@@ -30,10 +30,73 @@ require_once('PHPTMAPITestCase.php');
  */
 class TopicMapSystemTest extends PHPTMAPITestCase {
   
+  private $sys;
+  
+  /**
+   * @override
+   */
+  public function setUp() {
+    parent::setUp();
+    $this->sys = $this->sharedFixture;
+  }
+  
+  public function testTopicMapSystem() {
+    $this->assertTrue($this->sys instanceof TopicMapSystem);
+  }
+
   public function testTopicMap() {
     $this->assertTrue($this->topicMap instanceof TopicMap);
   }
-
-
+  
+  public function testLoad() {
+    $tm = $this->sys->getTopicMap(self::$tmLocator);
+    $this->assertNotNull($tm, 'Expected topic map!');
+    $id = $tm->getId();
+    $this->assertTrue(!empty($id), 'Expected internal identifier for topic map!');
+    $this->assertEquals($id, $this->topicMap->getId(), 'Expected identity!');
+  }
+  
+  public function testSameLocator() {
+    try {
+      $tm = $this->sys->createTopicMap(self::$tmLocator);
+      $this->fail('A topic map under the same storage address already exists!');
+    } catch (TopicMapExistsException $e) {
+      // no op.
+    }
+  }
+  
+  public function testCreateTopicMaps() {
+    $base = 'http://localhost/topicmaps/';
+    $tm1 = $this->sys->createTopicMap($base . 'map1');
+    $tm2 = $this->sys->createTopicMap($base . 'map2');
+    $tm3 = $this->sys->createTopicMap($base . 'map3');
+    $this->assertNotNull($tm1, 'Expected topic map!');
+    $this->assertNotNull($tm2, 'Expected topic map!');
+    $this->assertNotNull($tm3, 'Expected topic map!');
+    $id1 = $tm1->getId();
+    $id2 = $tm2->getId();
+    $id3 = $tm3->getId();
+    $this->assertTrue(!empty($id1), 'Expected internal identifier for topic map!');
+    $this->assertTrue(!empty($id2), 'Expected internal identifier for topic map!');
+    $this->assertTrue(!empty($id3), 'Expected internal identifier for topic map!');
+    $this->assertNotEquals($id1, $id2, 'Unexpected identity!');
+    $this->assertNotEquals($id1, $id3, 'Unexpected identity!');
+    $this->assertNotEquals($id2, $id3, 'Unexpected identity!');
+  }
+  
+  public function testRemoveTopicMaps() {
+    $base = 'http://localhost/topicmaps/';
+    $tm1 = $this->sys->createTopicMap($base . 'map1');
+    $tm2 = $this->sys->createTopicMap($base . 'map2');
+    $tm3 = $this->sys->createTopicMap($base . 'map3');
+    $this->assertNotNull($tm1, 'Expected topic map!');
+    $this->assertNotNull($tm2, 'Expected topic map!');
+    $this->assertNotNull($tm3, 'Expected topic map!');
+    $countBefore = count($this->sys->getLocators());
+    $tm3->remove();
+    $countAfter = count($this->sys->getLocators());
+    $this->assertEquals($countBefore-1, $countAfter, 'Expected ' . $countBefore-1 . 
+      ' topic maps!');
+  }
 }
 ?>
