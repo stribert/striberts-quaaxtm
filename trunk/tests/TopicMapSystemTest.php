95f1962b27cb61b88bfc4e138fe71d3bcf711012
joschmidt <joschmidt@11aa7465-d437-4035-867d-07b0d7ee7c12>
2009-04-27 21:16:14 +0000

    initial
    git-svn-id: https://quaaxtm.svn.sourceforge.net/svnroot/quaaxtm@70 11aa7465-d437-4035-867d-07b0d7ee7c12

---
+++ trunk/tests/TopicMapSystemTest.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * QuaaxTM is an implementation of PHPTMAPI which uses MySQL with InnoDB as 
+ * storage engine.
+ * 
+ * Copyright (C) 2008 Johannes Schmidt <phptmapi-discuss@lists.sourceforge.net>
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License as published by the Free Software 
+ * Foundation; either version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Lesser General Public License for more details.
+ *  
+ * You should have received a copy of the GNU Lesser General Public License along with this 
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, 
+ * Boston, MA 02111-1307 USA
+ */
+
+require_once('PHPTMAPITestCase.php');
+
+/**
+ * Topic map system tests.
+ *
+ * @package test
+ * @author Johannes Schmidt <phptmapi-discuss@lists.sourceforge.net>
+ * @license http://www.gnu.org/licenses/lgpl.html GNU LGPL
+ * @version $Id$
+ */
+class TopicMapSystemTest extends PHPTMAPITestCase {
+  
+  public function testTopicMap() {
+    $this->assertTrue($this->topicMap instanceof TopicMap);
+  }
+
+
+}
+?>
