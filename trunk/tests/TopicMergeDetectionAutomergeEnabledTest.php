07bb51e844aea2ec7396388d4dd6cafc6e6fcb6b
joschmidt <joschmidt@11aa7465-d437-4035-867d-07b0d7ee7c12>
2009-05-22 22:03:28 +0000

    added more tests
    git-svn-id: https://quaaxtm.svn.sourceforge.net/svnroot/quaaxtm@98 11aa7465-d437-4035-867d-07b0d7ee7c12

--- trunk/tests/TopicMergeDetectionAutomergeEnabledTest.php
+++ trunk/tests/TopicMergeDetectionAutomergeEnabledTest.php
@@ -33,10 +33,241 @@ require_once('PHPTMAPITestCase.php');
  */
 class TopicMergeDetectionAutomergeEnabledTest extends PHPTMAPITestCase {
 
+  private $automerge;
+  
+  /**
+   * @override
+   */
+  public function setUp() {
+    parent::setUp();
+    try {
+      $sys = $this->sharedFixture;
+      $this->automerge = (bool) $sys->getFeature('http://tmapi.org/features/automerge/');
+    } catch (FeatureNotRecognizedException $e) {
+      $this->automerge = false;
+    }
+  }
+  
+  /**
+   * @override
+   */
+  public function tearDown() {
+    parent::tearDown();
+    $this->automerge = false;
+  }
+
   public function testTopicMap() {
     $this->assertTrue($this->topicMap instanceof TopicMap);
   }
   
-
+  /**
+   * Tests if adding a duplicate subject identifier is detected.
+   */
+  public function testExistingSubjectIdentifier() {
+    if (!$this->automerge) return;
+    $tm = $this->topicMap;
+    $topic1 = $tm->createTopic();
+    $topic2 = $tm->createTopic();
+    $this->assertEquals(count($tm->getTopics()), 2, 'Expected 2 topics!');
+    $sid = 'http://phptmapi.sourceforge.net/';
+    $topic1->addSubjectIdentifier($sid);
+    $this->assertTrue(in_array($sid, $topic1->getSubjectIdentifiers()), 
+      'Expected a certain subject identifier!');
+    $this->assertEquals($topic1->getId(), $tm->getTopicBySubjectIdentifier($sid)->getId(), 
+      'Expected identity!');
+    $topic2->addSubjectIdentifier($sid);// merging takes place
+    $topics = $tm->getTopics();
+    if (count($topics) === 1) {
+      $topic = $topics[0];
+      $this->assertTrue(in_array($sid, $topic->getSubjectIdentifiers()), 
+        'Expected a certain subject identifier!');
+    } else {
+      $this->fail('Expected 1 topic!');
+    }
+  }
+  
+  /**
+   * Tests if adding a duplicate subject identifier on the same topic is ignored.
+   */
+  public function testExistingSubjectIdentifierLegal() {
+    if (!$this->automerge) return;
+    $tm = $this->topicMap;
+    $topic = $tm->createTopic();
+    $this->assertEquals(count($tm->getTopics()), 1, 'Expected 1 topic!');
+    $sid = 'http://phptmapi.sourceforge.net/';
+    $topic->addSubjectIdentifier($sid);
+    $this->assertTrue(in_array($sid, $topic->getSubjectIdentifiers()), 
+      'Expected a certain subject identifier!');
+    $this->assertEquals($topic->getId(), $tm->getTopicBySubjectIdentifier($sid)->getId(), 
+      'Expected identity!');
+    $topic->addSubjectIdentifier($sid);
+    $this->assertEquals(count($topic->getSubjectIdentifiers()), 1, 
+      'Expected 1 subject identifier!');
+  }
+  
+  /**
+   * Tests if adding a duplicate subject locator is detected.
+   */
+  public function testExistingSubjectLocator() {
+    if (!$this->automerge) return;
+    $tm = $this->topicMap;
+    $topic1 = $tm->createTopic();
+    $topic2 = $tm->createTopic();
+    $this->assertEquals(count($tm->getTopics()), 2, 'Expected 2 topics!');
+    $slo = 'http://phptmapi.sourceforge.net/';
+    $topic1->addSubjectLocator($slo);
+    $this->assertTrue(in_array($slo, $topic1->getSubjectLocators()), 
+      'Expected a certain subject locator!');
+    $this->assertEquals($topic1->getId(), $tm->getTopicBySubjectLocator($slo)->getId(), 
+      'Expected identity!');
+    $topic2->addSubjectLocator($slo);// merging takes place
+    $topics = $tm->getTopics();
+    if (count($topics) === 1) {
+      $topic = $topics[0];
+      $this->assertTrue(in_array($slo, $topic->getSubjectLocators()), 
+        'Expected a certain subject locator!');
+    } else {
+      $this->fail('Expected 1 topic!');
+    }
+  }
+  
+  /**
+   * Tests if adding a duplicate subject locator at the same topic is ignored.
+   */
+  public function testExistingSubjectLocatorLegal() {
+    if (!$this->automerge) return;
+    $tm = $this->topicMap;
+    $topic = $tm->createTopic();
+    $this->assertEquals(count($tm->getTopics()), 1, 'Expected 1 topic!');
+    $slo = 'http://phptmapi.sourceforge.net/';
+    $topic->addSubjectLocator($slo);
+    $this->assertTrue(in_array($slo, $topic->getSubjectLocators()), 
+      'Expected a certain subject locator!');
+    $this->assertEquals($topic->getId(), $tm->getTopicBySubjectLocator($slo)->getId(), 
+      'Expected identity!');
+    $topic->addSubjectLocator($slo);
+    $this->assertEquals(count($topic->getSubjectLocators()), 1, 
+      'Expected 1 subject locator!');
+  }
+  
+  /**
+   * Tests if adding an item identifier equals to a subject identifier is detected.
+   */
+  public function testExistingSubjectIdentifierAddItemIdentifier() {
+    if (!$this->automerge) return;
+    $tm = $this->topicMap;
+    $topic1 = $tm->createTopic();
+    $topic2 = $tm->createTopic();
+    $this->assertEquals(count($tm->getTopics()), 2, 'Expected 2 topics!');
+    $loc = 'http://phptmapi.sourceforge.net/';
+    $topic1->addSubjectIdentifier($loc);
+    $this->assertTrue(in_array($loc, $topic1->getSubjectIdentifiers()), 
+      'Expected a certain subject identifier!');
+    $this->assertEquals($topic1->getId(), $tm->getTopicBySubjectIdentifier($loc)->getId(), 
+      'Expected identity!');
+    $topic2->addItemIdentifier($loc);// merging takes place
+    $topics = $tm->getTopics();
+    if (count($topics) === 1) {
+      $topic = $topics[0];
+      $this->assertTrue(in_array($loc, $topic->getSubjectIdentifiers()), 
+        'Expected a certain subject identifier!');
+    } else {
+      $this->fail('Expected 1 topic!');
+    }
+  }
+  
+  /**
+   * Tests if adding an item identifier equals to a subject identifier 
+   * on the same topic is accepted.
+   */
+  public function testExistingSubjectIdentifierAddItemIdentifierLegal() {
+    if (!$this->automerge) return;
+    $tm = $this->topicMap;
+    $sid = 'http://phptmapi.sourceforge.net/';
+    $topic = $tm->createTopicBySubjectIdentifier($sid);
+    $this->assertEquals(count($tm->getTopics()), 1, 'Expected 1 topic!');
+    $this->assertEquals(count($topic->getSubjectIdentifiers()), 1, 
+      'Expected 1 subject identifier!');
+    $this->assertTrue(in_array($sid, $topic->getSubjectIdentifiers()), 
+      'Expected a certain subject identifier!');
+    $this->assertEquals(count($topic->getItemIdentifiers()), 0, 
+      'Unexpected item identifier!');
+    $this->assertEquals($topic->getId(), $tm->getTopicBySubjectIdentifier($sid)->getId(), 
+      'Expected identity!');
+    $this->assertNull($tm->getConstructByItemIdentifier($sid), 'Unexpected topic!');
+    $topic->addItemIdentifier($sid);
+    $this->assertEquals(count($topic->getSubjectIdentifiers()), 1, 
+      'Expected 1 subject identifier!');
+    $this->assertEquals(count($topic->getItemIdentifiers()), 1, 
+      'Expected 1 item identifier!');
+    $this->assertTrue(in_array($sid, $topic->getSubjectIdentifiers()), 
+      'Expected a certain subject identifier!');
+    $this->assertTrue(in_array($sid, $topic->getItemIdentifiers()), 
+      'Expected a certain item identifier!');
+    $this->assertEquals($topic->getId(), $tm->getTopicBySubjectIdentifier($sid)->getId(), 
+      'Expected identity!');
+    $this->assertEquals($topic->getId(), $tm->getConstructByItemIdentifier($sid)->getId(), 
+      'Expected identity!');
+  }
+  
+  /**
+   * Tests if adding a subject identifier equals to an item identifier is detected.
+   */
+  public function testExistingItemIdentifierAddSubjectIdentifier() {
+    if (!$this->automerge) return;
+    $tm = $this->topicMap;
+    $topic1 = $tm->createTopic();
+    $topic2 = $tm->createTopic();
+    $this->assertEquals(count($tm->getTopics()), 2, 'Expected 2 topics!');
+    $loc = 'http://phptmapi.sourceforge.net/';
+    $topic1->addItemIdentifier($loc);
+    $this->assertTrue(in_array($loc, $topic1->getItemIdentifiers()), 
+      'Expected a certain item identifier!');
+    $this->assertEquals($topic1->getId(), $tm->getConstructByItemIdentifier($loc)->getId(), 
+      'Expected identity!');
+    $topic2->addSubjectIdentifier($loc);// merging takes place
+    $topics = $tm->getTopics();
+    if (count($topics) === 1) {
+      $topic = $topics[0];
+      $this->assertTrue(in_array($loc, $topic->getItemIdentifiers()), 
+        'Expected a certain item identifier!');
+    } else {
+      $this->fail('Expected 1 topic!');
+    }
+  }
+  
+  /**
+   * Tests if adding a subject identifier equals to an item identifier 
+   * on the same topic is accepted.
+   */
+  public function testExistingItemIdentifierAddSubjectIdentifierLegal() {
+    if (!$this->automerge) return;
+    $tm = $this->topicMap;
+    $iid = 'http://phptmapi.sourceforge.net/';
+    $topic = $tm->createTopicByItemIdentifier($iid);
+    $this->assertEquals(count($tm->getTopics()), 1, 'Expected 1 topic!');
+    $this->assertEquals(count($topic->getItemIdentifiers()), 1, 
+      'Expected 1 item identifier!');
+    $this->assertTrue(in_array($iid, $topic->getItemIdentifiers()), 
+      'Expected a certain item identifier!');
+    $this->assertEquals(count($topic->getSubjectIdentifiers()), 0, 
+      'Unexpected subject identifier!');
+    $this->assertEquals($topic->getId(), $tm->getConstructByItemIdentifier($iid)->getId(), 
+      'Expected identity!');
+    $this->assertNull($tm->getTopicBySubjectIdentifier($iid), 'Unexpected topic!');
+    $topic->addSubjectIdentifier($iid);
+    $this->assertEquals(count($topic->getSubjectIdentifiers()), 1, 
+      'Expected 1 subject identifier!');
+    $this->assertEquals(count($topic->getItemIdentifiers()), 1, 
+      'Expected 1 item identifier!');
+    $this->assertTrue(in_array($iid, $topic->getSubjectIdentifiers()), 
+      'Expected a certain subject identifier!');
+    $this->assertTrue(in_array($iid, $topic->getItemIdentifiers()), 
+      'Expected a certain item identifier!');
+    $this->assertEquals($topic->getId(), $tm->getTopicBySubjectIdentifier($iid)->getId(), 
+      'Expected identity!');
+    $this->assertEquals($topic->getId(), $tm->getConstructByItemIdentifier($iid)->getId(), 
+      'Expected identity!');
+  }
 }
 ?>
