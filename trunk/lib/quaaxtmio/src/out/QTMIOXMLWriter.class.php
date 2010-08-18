5b16072c7dd436a23b9fa3ba167338a1a12d2b3c
joschmidt <joschmidt@11aa7465-d437-4035-867d-07b0d7ee7c12>
2010-08-18 10:39:53 +0000

    initial commit
    git-svn-id: https://quaaxtm.svn.sourceforge.net/svnroot/quaaxtm@316 11aa7465-d437-4035-867d-07b0d7ee7c12

---
+++ trunk/lib/quaaxtmio/src/out/QTMIOXMLWriter.class.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * QuaaxTMIO is the Topic Maps syntaxes serializer/deserializer library for QuaaxTM.
+ * 
+ * Copyright (C) 2010 Johannes Schmidt <joschmidt@users.sourceforge.net>
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License as published by the Free Software 
+ * Foundation; either version 3 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Lesser General Public License for more details.
+ *  
+ * You should have received a copy of the GNU Lesser General Public License along with this 
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, 
+ * Boston, MA 02111-1307 USA
+ */
+
+/**
+ * Extends the standard PHP XMLWriter. 
+ * 
+ * @package out
+ * @author Johannes Schmidt <joschmidt@users.sourceforge.net>
+ * @license http://www.gnu.org/licenses/lgpl.html GNU LGPL
+ * @version $Id$
+ */
+class QTMIOXMLWriter extends XMLWriter {
+  
+  const NL = "\n";
+  
+  /**
+   * Writes a new line.
+   * 
+   * @return void
+   */
+  public function writeNewLine() {
+    parent::text(self::NL);
+  }
+}
+?>
